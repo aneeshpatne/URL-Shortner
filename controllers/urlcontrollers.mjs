7df59d234c6dae75f7010d817d39c83da06e82f2
aneeshpatne <aneeshpatne@gmail.com>
2024-11-15 17:35:37 +0530

    feat: functions to handle URL shortening

---
+++ controllers/urlcontrollers.mjs
@@ -0,0 +1,29 @@
+import Url from "../models/Urls.mjs";
+import {URLShortner} from "../services/urlService.mjs";
+export const shortenURL = async (req, res) =>{
+    const {originalUrl} = req.body;
+    try{
+        const shortUrl = URLShortner();
+        const url = await Url.create({originalUrl, shortUrl});
+        res.json({shortUrl: url.shortUrl});
+    }
+    catch(err){
+        res.status(500).json({error: 'Server Error'});
+    }
+};
+export const expandURL = async (req, res) =>{
+    const {shortUrl} = req.params;
+    try{
+        const url = await Url.findOne({shortUrl});
+        if(url){
+            return res.redirect(url.originalUrl);   
+        }
+        else{
+            return res.status(404).json({error: 'URL not found'});
+        }
+
+    }
+    catch(error){
+        res.status(500).json({error: 'Server Error'});
+    }
+};
