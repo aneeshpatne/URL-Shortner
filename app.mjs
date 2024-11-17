1bb38715892ac288baff0f73e8beb831348e6361
aneeshpatne <aneeshpatne@gmail.com>
2024-11-17 21:23:09 +0530

    chore: cors restriction

--- app.mjs
+++ app.mjs
@@ -4,6 +4,11 @@ import router from './routes/urlRoutes.mjs';
 import  errorHandler from './middleware/errorHandler.mjs';
 const app = express();
 app.use(cors());
+const corsOptions = {
+    origin: 'https://url-shortner-frontend-rust.vercel.app',
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/', router);
 app.use(errorHandler);
